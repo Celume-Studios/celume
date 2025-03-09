594f8c633fb6b295738c05caed05ca062155d179
ashwazz <127042776+ashwazz@users.noreply.github.com>
2025-03-09 17:44:21 +0530

    Changes to be committed: 	modified:   src/components/Hero.jsx 	modified:   src/styles/Hero.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,11 +41,15 @@ const Hero = () => {
           <div className="logo">
             <img src={logo} alt="Celume Studios" />
           </div>
-          <div className="nav-links">
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
-            <a href="#portfolio">Portfolio</a>
-            <a href="#contact">Contact Us</a>
+          <div className="nav-center">
+            <div className="nav-links">
+              <a href="#about">About</a>
+              <a href="#services">Services</a>
+              <a href="#portfolio">Portfolio</a>
+              <a href="#contact">Contact Us</a>
+            </div>
+          </div>
+          <div className="nav-right">
             <button className="get-started-btn">Get Started</button>
           </div>
         </nav>
