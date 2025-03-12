bc1edcf6a82beb027da24bc0bee446009d70701a
Vidhyadhar Reddy kudumula <kudumala123@gmail.com>
2025-03-12 18:28:24 +0530

    Products margins

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -25,9 +25,9 @@ const Products = () => {
                 <div className="tag">Development Project</div>
                 <h3>Celume 360</h3>
                 <p>Celume 360 is the most easiest to use tool to build custom virtual tour websites in seconds</p>
-                <div className="card-description">
+                {/* <div className="card-description">
                   Virtual Tours Accessible on the widest range of devices from anywhere with a simple browser
-                </div>
+                </div> */}
               </div>
               <div className="card-image">
                 <img src={celume360Img} alt="Celume 360 Interface" />
@@ -42,9 +42,9 @@ const Products = () => {
                 <div className="tag">Development Project</div>
                 <h3>Celume Datamachine</h3>
                 <p>Data machine skips all the hard work that goes into data collection and makes it a simple one click procedure</p>
-                <div className="card-description">
+                {/* <div className="card-description">
                   Upcoming - Extracting and mining data made easy
-                </div>
+                </div> */}
               </div>
               <div className="card-image">
                 <img src={datamachineImg} alt="Datamachine Interface" />
