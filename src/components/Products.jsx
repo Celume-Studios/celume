ec44fb37e445cad231a615fca8708f1ba2a81243
ashwazz <127042776+ashwazz@users.noreply.github.com>
2025-03-13 14:24:21 +0530

    linking Our Portfolio video and small fixes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -11,7 +11,7 @@ const Products = () => {
             <p className="subtitle">Here is a host of studio - developed products</p>
           </div>
           <div className="header-right">
-            <button className="portfolio-btn">All Portfolio</button>
+            <button className="portfolio-btn" onClick={() => window.open('https://youtu.be/2SK0YNDQmR8', '_blank')}>All Portfolio</button>
           </div>
         </div>
 
@@ -20,7 +20,7 @@ const Products = () => {
           <div className="product-card">
             <div className="product-card-inner">
               <div className="card-content">
-                <div className="tag">Development Project</div>
+                <div className="tag">Download Now</div>
                 <h3>Celume 360</h3>
                 <p>Celume 360 is the most easiest to use tool to build custom virtual tour websites in seconds</p>
                 {/* <div className="card-description">
@@ -37,7 +37,7 @@ const Products = () => {
           <div className="product-card">
             <div className="product-card-inner">
               <div className="card-content">
-                <div className="tag">Development Project</div>
+                <div className="tag">Upcoming</div>
                 <h3>Celume Datamachine</h3>
                 <p>Data machine skips all the hard work that goes into data collection and makes it a simple one click procedure</p>
                 {/* <div className="card-description">
