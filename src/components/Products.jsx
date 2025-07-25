e7c9f2ef762f92f66f041e6fef05f285370031d5
Vidhyadhar Reddy kudumula <kudumala123@gmail.com>
2025-07-25 20:09:58 +0530

    Products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -23,9 +23,7 @@ const Products = () => {
                 <div className="tag">Under Development</div>
                 <h3>Celume 360</h3>
                 <p>Celume 360 is the most easiest to use tool to build custom virtual tour websites in seconds</p>
-                {/* <div className="card-description">
-                  Virtual Tours Accessible on the widest range of devices from anywhere with a simple browser
-                </div> */}
+                
               </div>
               <div className="card-image">
                 <img src="/images/celume360(1).png" alt="Celume 360 Interface" />
@@ -34,21 +32,24 @@ const Products = () => {
           </div>
 
           {/* Call Nova Card */}
+          <button className="product-card-button" onClick={() => window.location.href = 'https://demo.celumestudios.com'}>
           <div className="product-card">
             <div className="product-card-inner">
               <div className="card-content">
-                <div className="tag">Beta</div>
+                <div className="tag">launching soon</div>
                 <h3>Celume Call Nova</h3>
                 <p>Call Nova is an human like ai voice assistant that can be used to receive calls and note down the important details</p>
                 {/* <div className="card-description">
                   Upcoming - Extracting and mining data made easy
                 </div> */}
+
               </div>
               <div className="card-image">
-                <img src="/images/callnova.jpg" alt="Celume Call Nova Interface" />
+                <img src="/images/dashboardpreview.png" alt="Celume Call Nova Interface" />
               </div>
             </div>
           </div>
+            </button>
         </div>
       </div>
     </section>
